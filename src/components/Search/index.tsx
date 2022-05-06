9ff9373b2b3dce6b3834876823bc963e45375e4f
ykhost <andrew.heringer1337@gmail.com>
2022-05-06 20:38:57 -0300

    added search component and style

---
+++ src/components/Search/index.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
+
+type ItemProps = {
+  id: number;
+  name: string;
+}
+
+
+function App() {
+  const items: ItemProps[] = [
+    {
+      id: 0,
+      name: 'Cobol'
+    },
+    {
+      id: 1,
+      name: 'JavaScript'
+    },
+    {
+      id: 2,
+      name: 'Basic'
+    },
+    {
+      id: 3,
+      name: 'PHP'
+    },
+    {
+      id: 4,
+      name: 'Java'
+    }
+  ]
+
+  const handleOnSearch = (string: string, results: any) => {
+    // onSearch will have as the first callback parameter
+    // the string searched and for the second the results.
+    console.log(string, results)
+  }
+
+  const handleOnHover = (result: any) => {
+    // the item hovered
+    console.log(result)
+  }
+
+  const handleOnSelect = (item: ItemProps) => {
+    // the item selected
+    console.log(item)
+  }
+
+  const handleOnFocus = () => {
+    console.log('Focused')
+  }
+
+  const formatResult = (item: ItemProps) => {
+    return (
+      <>
+        <span style={{ display: 'block', textAlign: 'left' }}>{item.name}</span>
+      </>
+    )
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={{ width: 400 }}>
+          <ReactSearchAutocomplete
+            items={items}
+            onSearch={handleOnSearch}
+            onHover={handleOnHover}
+            onSelect={handleOnSelect}
+            onFocus={handleOnFocus}
+            autoFocus
+            formatResult={formatResult}
+          />
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
